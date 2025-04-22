38e61725af897d29741589ef9b4d70fb68b05900
Akbar <akbarsafarov.as@gmail.com>
2025-04-23 00:51:15 +0500

    done

---
+++ js/main.js
@@ -0,0 +1,83 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sliderItems = document.querySelectorAll('.list_items_inner .item_block .slider_block_wr');
+
+    if (sliderItems.length > 0) {
+        sliderItems.forEach(function(sliderWrapper) {
+            const slider = sliderWrapper.querySelector('.swiper');
+            if (!slider) return;
+
+            const sliderId = slider.dataset.id;
+            const arrow = slider.dataset.arrow;
+
+            if (!sliderId || !arrow) return;
+
+            const sliderClass = '.' + sliderId;
+
+            new Swiper(sliderClass, {
+                loop: true,
+                slidesPerView: 1,
+                loopedSlides: 1,
+                navigation: {
+                    nextEl: '.swiper-' + arrow + '-next',
+                    prevEl: '.swiper-' + arrow + '-prev',
+                },
+                lazy: true
+            });
+        });
+    }
+
+    const slidertag = document.querySelector('.mySwiper_tag');
+    let swiperInstance = null;
+
+    function initTagSlider() {
+        if (!slidertag) return;
+
+        const slidertagId = slidertag.dataset.id;
+        const arrowTag = slidertag.dataset.arrow;
+        const sliderClassTag = '.' + slidertagId;
+
+        if (window.innerWidth <= 767 && !swiperInstance) {
+            swiperInstance = new Swiper(sliderClassTag, {
+                loop: true,
+                slidesPerView: 'auto',
+                spaceBetween: 20,
+                loopedSlides: 1,
+                navigation: {
+                    nextEl: '.swiper-' + arrowTag + '-next',
+                    prevEl: '.swiper-' + arrowTag + '-prev',
+                },
+                lazy: true
+            });
+        } else if (window.innerWidth > 767 && swiperInstance) {
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
+        }
+    }
+
+    initTagSlider();
+
+    window.addEventListener('resize', initTagSlider);
+
+    const accorItems = document.querySelectorAll('.accor_item .accor_name');
+
+    if(accorItems.length > 0) {
+        accorItems.forEach(function(item){
+            item.addEventListener('click', function () {
+                const currentBlock = this.parentElement;
+
+                document.querySelectorAll('.accor_item').forEach(block => {
+                    if (block !== currentBlock) {
+                        block.classList.remove('opened');
+                    }
+                });
+                currentBlock.classList.toggle('opened');
+            });
+        });
+    }
+
+    $('.type-phone input').mask("+7 (999) 999-99-99");
+});
+
+
